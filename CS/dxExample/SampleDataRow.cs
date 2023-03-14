9e299f2779a446c6c52496db7e2234a809763c2e
AndreySlabov <andrey.slabov@devexpress.com>
2023-03-14 18:42:42 +0400

    format

--- CS/dxExample/SampleDataRow.cs
+++ CS/dxExample/SampleDataRow.cs
@@ -1,19 +1,4 @@
-﻿// Developer Express Code Central Example:
-// How to create a custom GridControl that represents columns horizontally in a way similar to the WinForms VerticalGrid control
-// 
-// This example demonstrates how to create a GridControl descendant with
-// horizontally oriented columns.
-// To use it, simply add the
-// VerticalGridControl.xaml and VerticalGridControl.xaml.cs files in your
-// project.
-// 
-// In addition, this example demonstrates how to customize grid cells
-// using data templates.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E4630
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace dxExample {
